a7b2705d1228cbbc91f0c1fd78057c71b3656ebc
rooneyish <ilustraterp@gmail.com>
2025-08-22 19:55:25 +0545

    Add Activation Function

---
+++ mnist_project/src/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def softmax(z):
+    exp_z = np.exp(z-np.max(z, axis=1, keepdims= True))
+    return exp_z/np.sum(exp_z, axis=1, keepdims= True)
+
+def leaky_relu(z, alpha = 0.01):
+    return np.maximum(alpha*z, z)
+
+def derivative_leaky_relu(z, alpha = 0.01):
+    return (z > 0).astype(float) + alpha * (z <= 0).astype(float)
